be56c76b16bc313ef4533e3e007ef788b73c6796
aeabbott <amandaeabbott@gmail.com>
2017-04-06 11:18:58 -0400

    Sign Up Feature- Barebones/works but looks gross
    1. Added auth folder under scripts
    2. Added api file, events file and ui file under auth file
    3. Added code in to get sign up to register user on backend
    4. Added curl scripts folder for validation of backend before coding front end

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,15 @@
+'use strict'
+const store = require('../store')
+
+const signUpSuccess = (data) => {
+  console.log(data)
+}
+
+const signUpFailure = (error) => {
+  console.error(error)
+}
+
+module.exports = {
+  signUpSuccess,
+  signUpFailure
+}
